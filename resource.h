e202740485106b4930724f1baa02340a17b61c52
LeQuy <quygia128.cin1@gmail.com>
2013-08-18 22:37:41 +0700

    git add

--- resource.h
+++
@@ -1,9 +0,0 @@
-#ifndef IDC_STATIC
-#define IDC_STATIC (-1)
-#endif
-
-#define IDD_DIALOG1                             100
-#define IDC_EDIT1                               1001
-#define IDC_BUTTON1                             1002
-#define IDC_BUTTON3                             1004
-#define IDC_BUTTON2                             1005
